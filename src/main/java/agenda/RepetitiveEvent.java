647632b9a9a3ad876176e7185f32dd6efd420375
Damien Lopez <damien-lopez@etud.univ-jfc.fr>
2022-11-17 11:50:13 +0100

    RepetitiveEvent essai

--- src/main/java/agenda/RepetitiveEvent.java
+++ src/main/java/agenda/RepetitiveEvent.java
@@ -3,6 +3,7 @@ package agenda;
 import java.util.*;
 import java.time.*;
 import java.time.temporal.ChronoUnit;
+import java.time.chrono.ChronoLocalDate;
 
 /**
  * Description : A repetitive Event
@@ -25,10 +26,14 @@ public class RepetitiveEvent extends Event {
     private LocalDateTime start;
     private Duration duration;
     private ChronoUnit frequency;
+    private HashSet<LocalDate> lRepEvent;
 
     public RepetitiveEvent(String title, LocalDateTime start, Duration duration, ChronoUnit frequency) {
         super(title, start, duration);
         this.frequency = frequency;
+        this.lRepEvent = new HashSet<LocalDate>();
+       
+
     }
 
     /**
@@ -37,7 +42,32 @@ public class RepetitiveEvent extends Event {
      * @param date the event will not occur at this date
      */
     public void addException(LocalDate date) {
+       /*  LocalDate tmp = this.start.toLocalDate();
+        Duration du = frequency.getDuration(); */
+        this.lRepEvent.add(date);
+    }
+
+    @Override
+    public boolean isInDay(LocalDate aDay) {
         
+        if(this.lRepEvent.contains(aDay)){
+            return false;
+        }
+        if(this.frequency == ChronoUnit.DAYS){
+           return true; 
+        }
+        if(this.frequency == ChronoUnit.WEEKS){
+            
+            
+         }
+         
+        if((aDay.getDayOfYear() == this.getStart().getDayOfYear() &&  aDay.getYear() == this.getStart().getYear()) 
+        || (aDay.isAfter(ChronoLocalDate.from(this.getStart())) && aDay.isBefore(ChronoLocalDate.from(this.getStart().plus(this.getDuration())))) 
+        || (aDay.getDayOfYear() == this.getStart().plus(this.getDuration()).getDayOfYear() && aDay.getYear() == this.getStart().plus(this.getDuration()).getYear())){
+        return true;
+    }
+
+    return false;
     }
 
     /**
@@ -45,7 +75,9 @@ public class RepetitiveEvent extends Event {
      * @return the type of repetition
      */
     public ChronoUnit getFrequency() {
-        return frequency;
+        return this.frequency;
+
+        
     }
 
 }
